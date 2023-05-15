bd8b51887bc927b53ff0e12e2cd12c597287628e
Kurt Böhm <kurbo96@gmail.com>
2023-05-15 18:43:06 +0200

    Add an apply sink

--- include/thesauros/utility/static-ranges/sinks.hpp
+++ include/thesauros/utility/static-ranges/sinks.hpp
@@ -2,6 +2,7 @@
 #define INCLUDE_THESAUROS_UTILITY_STATIC_RANGES_SINKS_HPP
 
 #include "sinks/all-different.hpp"
+#include "sinks/apply.hpp"
 #include "sinks/for-each.hpp"
 #include "sinks/reduce.hpp"
 #include "sinks/to-array.hpp"
---
+++ include/thesauros/utility/static-ranges/sinks/apply.hpp
@@ -0,0 +1,34 @@
+#ifndef INCLUDE_THESAUROS_UTILITY_STATIC_RANGES_SINKS_APPLY_HPP
+#define INCLUDE_THESAUROS_UTILITY_STATIC_RANGES_SINKS_APPLY_HPP
+
+#include <cstddef>
+#include <utility>
+
+#include "thesauros/optimization.hpp"
+#include "thesauros/utility/static-ranges/definitions/concepts.hpp"
+#include "thesauros/utility/static-ranges/definitions/size.hpp"
+
+namespace thes::star {
+template<typename TOp>
+struct ApplyGenerator {
+  TOp op;
+
+  template<typename TRange>
+  constexpr decltype(auto) operator()(TRange&& range) const {
+    constexpr std::size_t size = thes::star::size<TRange>;
+    return [this, &range]<std::size_t... tIdxs>(
+             std::index_sequence<tIdxs...> /*idxs*/) -> decltype(auto) {
+      return op(get_at<tIdxs>(range)...);
+    }(std::make_index_sequence<size>{});
+  }
+};
+template<typename TOp>
+struct ConsumerGeneratorTrait<ApplyGenerator<TOp>> : public std::true_type {};
+
+template<typename TOp>
+inline constexpr ApplyGenerator<TOp> for_each(TOp&& op) {
+  return {std::forward<TOp>(op)};
+}
+} // namespace thes::star
+
+#endif // INCLUDE_THESAUROS_UTILITY_STATIC_RANGES_SINKS_APPLY_HPP
