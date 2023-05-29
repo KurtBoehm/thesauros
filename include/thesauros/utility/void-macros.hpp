f53f5a1a2abc86cd64b9b595eb2476afe8765493
Kurt Böhm <kurbo96@gmail.com>
2023-05-29 19:28:49 +0200

    Add void macros

---
+++ include/thesauros/utility/void-macros.hpp
@@ -0,0 +1,21 @@
+#ifndef INCLUDE_THESAUROS_UTILITY_VOID_MACROS_HPP
+#define INCLUDE_THESAUROS_UTILITY_VOID_MACROS_HPP
+
+namespace thes {
+#define THES_APPLY_VALUED_RETURN(TYPE, EXPR) \
+  if constexpr (std::is_void_v<TYPE>) { \
+    EXPR; \
+  } else { \
+    decltype(auto) value = EXPR; \
+    if (value.has_value()) { \
+      return value; \
+    } \
+  }
+
+#define THES_RETURN_EMPTY_OPTIONAL(TYPE) \
+  if constexpr (!std::is_void_v<TYPE>) { \
+    return TYPE{}; \
+  }
+} // namespace thes
+
+#endif // INCLUDE_THESAUROS_UTILITY_VOID_MACROS_HPP
