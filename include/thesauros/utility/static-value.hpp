ea09b4b20d0837581acd089bc412b087a563bf2d
Kurt Böhm <kurbo96@gmail.com>
2023-06-04 18:43:16 +0200

    Add a static value concept

--- include/thesauros/utility/static-value.hpp
+++ include/thesauros/utility/static-value.hpp
@@ -1,6 +1,8 @@
 #ifndef INCLUDE_THESAUROS_UTILITY_STATIC_VALUE_HPP
 #define INCLUDE_THESAUROS_UTILITY_STATIC_VALUE_HPP
 
+#include <type_traits>
+
 namespace thes {
 template<typename T, T tValue>
 struct StaticValue {
@@ -22,6 +24,13 @@ template<typename T, T tValue>
 inline constexpr StaticValue<T, tValue> static_value{};
 template<auto tValue>
 inline constexpr StaticAuto<tValue> static_auto{};
+
+template<typename TStaticValue>
+struct AnyStaticValueTrait : public std::false_type {};
+template<typename T, T tValue>
+struct AnyStaticValueTrait<StaticValue<T, tValue>> : public std::true_type {};
+template<typename TStaticValue>
+concept AnyStaticValue = AnyStaticValueTrait<TStaticValue>::value;
 } // namespace thes
 
 #endif // INCLUDE_THESAUROS_UTILITY_STATIC_VALUE_HPP
