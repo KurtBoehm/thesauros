a80d87852be4ea8a9e1712bc7bbc396e632bbd23
Kurt Böhm <kurbo96@gmail.com>
2023-06-09 22:10:03 +0200

    Add VoidStorage

---
+++ include/thesauros/utility/void-storage.hpp
@@ -0,0 +1,30 @@
+#ifndef INCLUDE_THESAUROS_UTILITY_VOID_STORAGE_HPP
+#define INCLUDE_THESAUROS_UTILITY_VOID_STORAGE_HPP
+
+#include "thesauros/utility/empty.hpp"
+
+namespace thes {
+template<typename T>
+struct VoidStorageTrait {
+  using Type = T;
+  using Cref = const T&;
+  using Rvalue = const T&;
+};
+template<>
+struct VoidStorageTrait<void> {
+  using Type = thes::Empty;
+  using Cref = void;
+  using Rvalue = thes::Empty;
+};
+
+template<typename T>
+using VoidStorage = VoidStorageTrait<T>::Type;
+template<typename T>
+using VoidStorageCref = VoidStorageTrait<T>::Cref;
+template<typename T>
+inline constexpr VoidStorageTrait<T>::Rvalue void_storage_rvalue(const T& value) {
+  return value;
+}
+} // namespace thes
+
+#endif // INCLUDE_THESAUROS_UTILITY_VOID_STORAGE_HPP
