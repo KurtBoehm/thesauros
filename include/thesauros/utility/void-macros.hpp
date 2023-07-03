2ff2ebc8bfb7e1bdb0dd9112be848d08b9bf9f09
Kurt Böhm <kurbo96@gmail.com>
2023-07-03 21:59:45 +0200

    Extend the void macros

--- include/thesauros/utility/void-macros.hpp
+++ include/thesauros/utility/void-macros.hpp
@@ -1,15 +1,22 @@
 #ifndef INCLUDE_THESAUROS_UTILITY_VOID_MACROS_HPP
 #define INCLUDE_THESAUROS_UTILITY_VOID_MACROS_HPP
 
+#include <boost/preprocessor.hpp>
+
 namespace thes {
-#define THES_APPLY_VALUED_RETURN(TYPE, EXPR) \
+#define THES_APPLY_VALUED_RETURN_EX(TYPE, EXPR, AFTER) \
   if constexpr (std::is_void_v<TYPE>) { \
     EXPR; \
+    BOOST_PP_REMOVE_PARENS(AFTER) \
   } else { \
-    if (TYPE value = EXPR; value.has_value()) { \
+    TYPE value = EXPR; \
+    BOOST_PP_REMOVE_PARENS(AFTER) \
+    if (value.has_value()) { \
       return value; \
     } \
   }
+
+#define THES_APPLY_VALUED_RETURN(TYPE, EXPR) THES_APPLY_VALUED_RETURN_EX(TYPE, EXPR, ())
 
 #define THES_RETURN_EMPTY_OPTIONAL(TYPE) \
   if constexpr (!std::is_void_v<TYPE>) { \
