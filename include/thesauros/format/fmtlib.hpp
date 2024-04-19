704d2df12b8d9e3fa91efa99d5f55cb5e8afb040
Kurt Böhm <kurbo96@gmail.com>
2024-04-19 11:10:05 +0200

    Extend fmtlib

--- include/thesauros/format/fmtlib.hpp
+++ include/thesauros/format/fmtlib.hpp
@@ -5,5 +5,6 @@
 #include <fmt/core.h> // IWYU pragma: export
 #include <fmt/format.h> // IWYU pragma: export
 #include <fmt/ranges.h> // IWYU pragma: export
+#include <fmt/std.h> // IWYU pragma: export
 
 #endif // INCLUDE_THESAUROS_FORMAT_FMTLIB_HPP
