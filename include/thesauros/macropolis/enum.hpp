6a8f1c7b76f8746897efbd5f739bfb886c369c14
Kurt Böhm <kurbo96@gmail.com>
2026-02-02 22:11:21 +0100

    Add an additional include for the Macropolis `enum` tools

--- include/thesauros/macropolis/enum.hpp
+++ include/thesauros/macropolis/enum.hpp
@@ -16,8 +16,9 @@
 #include "thesauros/concepts/type-traits.hpp"
 #include "thesauros/macropolis/helpers.hpp"
 #include "thesauros/static-ranges.hpp"
+#include "thesauros/string/static-string.hpp" // IWYU pragma: keep
 #include "thesauros/types/value-tag.hpp"
-#include "thesauros/utility/tuple.hpp"
+#include "thesauros/utility/tuple.hpp" // IWYU pragma: keep
 
 namespace thes {
 template<auto tValue, auto tName, auto tSerialName>
