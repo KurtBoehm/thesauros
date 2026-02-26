4c3f7a526203810e65749060fb491c7778b88d6d
Kurt Böhm <kurbo96@gmail.com>
2026-02-26 11:12:19 +0100

    Add an offset uniform index segmenter

--- include/thesauros/utility/index-segmentation.hpp
+++ include/thesauros/utility/index-segmentation.hpp
@@ -24,37 +24,37 @@ struct UniformIndexSegmenter {
   using Segment = TSegment;
   using Shared = Union<Size, Segment>;
 
-  constexpr UniformIndexSegmenter(Size size, Segment segment_num)
-      : size_(size), segment_num_(segment_num), div_(static_cast<Size>(size / segment_num)),
-        mod_(static_cast<Size>(size % segment_num)) {}
+  constexpr UniformIndexSegmenter(Size size, Segment segment_num) noexcept
+      : size_{size}, segment_num_{segment_num}, div_{Size(size / segment_num)},
+        mod_{Size(size % segment_num)} {}
 
-  [[nodiscard]] constexpr Size segment_start(const Segment segment) const {
+  [[nodiscard]] constexpr Size segment_start(const Segment segment) const noexcept {
     assert(segment <= segment_num_);
     return static_cast<Size>(segment * div_ + std::min<Shared>(mod_, segment));
   }
-  [[nodiscard]] constexpr Size segment_end(const Segment segment) const {
-    return segment_start(segment + 1);
+  [[nodiscard]] constexpr Size segment_end(const Segment segment) const noexcept {
+    return segment_start(Segment(segment + 1));
   }
 
-  [[nodiscard]] constexpr auto segment_range(const Segment segment) const {
+  [[nodiscard]] constexpr IotaRange<Size> segment_range(const Segment segment) const noexcept {
     return range(segment_start(segment), segment_end(segment));
   }
 
-  [[nodiscard]] constexpr Segment segment_of(Size index) const {
-    const Size ref = mod_ * (div_ + 1);
+  [[nodiscard]] constexpr Segment segment_of(Size index) const noexcept {
+    const Size ref = Size(mod_ * (div_ + 1));
     if (index <= ref) {
       // index / (div + 1)
-      return index / div_div1_;
+      return Segment(index / div_div1_);
     }
     // mod + (index - ref) / div
     assert(div_div_.has_value());
-    return mod_ + (index - ref) / *div_div_;
+    return Segment(mod_ + (index - ref) / *div_div_);
   }
 
-  [[nodiscard]] constexpr Size size() const {
+  [[nodiscard]] constexpr Size size() const noexcept {
     return size_;
   }
-  [[nodiscard]] constexpr Segment segment_num() const {
+  [[nodiscard]] constexpr Segment segment_num() const noexcept {
     return segment_num_;
   }
 
@@ -71,30 +71,59 @@ private:
 };
 
 template<typename TSize, typename TSegment>
+struct OffsetUniformIndexSegmenter : public UniformIndexSegmenter<TSize, TSegment> {
+  using Uniform = UniformIndexSegmenter<TSize, TSegment>;
+  using Size = TSize;
+  using Segment = TSegment;
+
+  constexpr OffsetUniformIndexSegmenter(Size offset, Size size, Segment segment_num) noexcept
+      : Uniform{size, segment_num}, offset_{offset} {}
+
+  [[nodiscard]] constexpr Size segment_start(const Segment segment) const noexcept {
+    return Uniform::segment_start(segment) + offset_;
+  }
+  [[nodiscard]] constexpr Size segment_end(const Segment segment) const noexcept {
+    return Uniform::segment_end(segment) + offset_;
+  }
+
+  [[nodiscard]] constexpr IotaRange<Size> segment_range(const Segment segment) const noexcept {
+    return range(segment_start(segment), segment_end(segment));
+  }
+
+  [[nodiscard]] constexpr Segment segment_of(Size index) const noexcept {
+    assert(index >= offset_);
+    return Uniform::segment_of(Size(index - offset_));
+  }
+
+private:
+  Size offset_;
+};
+
+template<typename TSize, typename TSegment>
 struct BlockedIndexSegmenter {
   using Size = TSize;
   using Segment = TSegment;
 
-  constexpr BlockedIndexSegmenter(Size size, Segment segment_num, Size block_size)
+  constexpr BlockedIndexSegmenter(Size size, Segment segment_num, Size block_size) noexcept
       : size_(size), block_size_(block_size), block_seg_(div_ceil(size, block_size), segment_num) {}
 
-  [[nodiscard]] constexpr Size segment_start(const Segment segment) const {
+  [[nodiscard]] constexpr Size segment_start(const Segment segment) const noexcept {
     return std::min(saturate_cast<Size>(block_size_ * block_seg_.segment_start(segment)), size_);
   }
-  [[nodiscard]] constexpr Size segment_end(const Segment segment) const {
-    return segment_start(segment + 1);
+  [[nodiscard]] constexpr Size segment_end(const Segment segment) const noexcept {
+    return segment_start(Segment(segment + 1));
   }
-  [[nodiscard]] constexpr auto segment_range(const Segment segment) const {
+  [[nodiscard]] constexpr auto segment_range(const Segment segment) const noexcept {
     return range(segment_start(segment), segment_end(segment));
   }
 
-  [[nodiscard]] constexpr Size size() const {
+  [[nodiscard]] constexpr Size size() const noexcept {
     return size_;
   }
-  [[nodiscard]] constexpr Segment segment_num() const {
+  [[nodiscard]] constexpr Segment segment_num() const noexcept {
     return block_seg_.segment_num();
   }
-  [[nodiscard]] constexpr Size block_size() const {
+  [[nodiscard]] constexpr Size block_size() const noexcept {
     return block_size_;
   }
 
