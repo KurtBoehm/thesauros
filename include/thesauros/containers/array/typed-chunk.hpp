ce270b5c93c460562cb842723a1410343e4b49ec
Kurt Böhm <kurbo96@gmail.com>
2026-08-13 20:00:33 +0200

    Remove the ugly const-correctness work-around in `TypedChunk` again

--- include/thesauros/containers/array/typed-chunk.hpp
+++ include/thesauros/containers/array/typed-chunk.hpp
@@ -74,10 +74,6 @@ struct TypedChunk {
   [[nodiscard]] constexpr auto data(this auto&& self) {
     return const_or_mut<decltype(self)>(self.begin_);
   }
-  /** Allows users to skirt const-correctness */
-  [[nodiscard]] constexpr Value* mutable_data(this auto&& self) {
-    return self.begin_;
-  }
 
   [[nodiscard]] constexpr Size size() const {
     return static_cast<Size>(end_ - begin_);
